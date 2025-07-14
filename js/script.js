ac1bdd92225f00640fed1ba16bacdbec5b59be73
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-07-14 16:31:46 +0430

    Dynamically load footer content from external HTML file

--- js/script.js
+++ js/script.js
@@ -5,4 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((data) => {
       document.getElementById("header").innerHTML = data;
     });
+
+  // Load footer
+  fetch("layout/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
+      setCurrentYear();
+    });
 });
