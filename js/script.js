ec2ca0597edbe61577b026458107f18754ddfefb
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-07-26 18:01:48 +0430

    Add signup form validation and error handling for name, email, and password

--- js/script.js
+++ js/script.js
@@ -43,3 +43,50 @@ function toggleTheme() {
     icon.classList.add("bi-moon-stars-fill");
   }
 }
+
+///////////////////////////////////////////////////////////
+// Sign-Up Form Handler
+function signupFormHandler() {
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const nameField = document.getElementById("name");
+    const emailField = document.getElementById("email");
+    const passwordField = document.getElementById("password");
+
+    const name = nameField.value.trim();
+    const email = emailField.value.trim();
+    const password = passwordField.value.trim();
+
+    document.querySelectorAll(".error-message").forEach((el) => el.remove());
+    [nameField, emailField, passwordField].forEach((field) =>
+      field.classList.remove("border-danger")
+    );
+
+    let valid = true;
+
+    if (name === "") {
+      showError(nameField, "Please Enter a Name.");
+      valid = false;
+    }
+    if (email === "") {
+      showError(emailField, "Please Enter an Email.");
+      valid = false;
+    }
+    if (password === "" || password.length < 6) {
+      showError(passwordField, "Password must be at least 6 characters long.");
+      valid = false;
+    }
+
+    if (valid) {
+      // showSuccessModal(); // Show success modal
+      setTimeout(() => {
+        window.location.href = "login.html";
+        alert("Your account has been created successfully!");
+      }, 3000);
+
+      loginFormHandler();
+    }
+  });
+}
